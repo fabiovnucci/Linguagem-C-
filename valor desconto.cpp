45703f21049215d868f8a29d531ad8125b1c22bf
fabiovnucci <fabiovnucci@gmail.com>
2022-09-10 20:13:20 -0300

    valor desconto

---
+++ valor desconto.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+float valor , desconto, valorDesconto;
+string nomeProduto;
+cout << "Digite o nome do produto que recebera o desconto : ";
+cin >> nomeProduto;
+
+cout << "Digite o valor do produto: R$";
+cin >> valor;
+
+desconto = valor *  0.3 ;
+valorDesconto = valor - desconto;
+
+
+cout << "O valor do " << nomeProduto << " com desconto e de : R$" << valorDesconto ;
+
+
+}
