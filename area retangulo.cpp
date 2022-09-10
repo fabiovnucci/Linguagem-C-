bdfd874f9141ebc6a8120cc5d7ed9a824d844b68
fabiovnucci <fabiovnucci@gmail.com>
2022-09-10 20:03:47 -0300

    area

---
+++ area retangulo.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    float base, altura, area;
+
+
+cout << "Digite a base do retangulo: ";
+cin >> base;
+
+cout << "Digite a altura do retangulo: ";
+cin >> altura ;
+
+area = base * altura;
+
+cout << "A area do retangulo e de : " << area << " cm";
+
+
+
+}
