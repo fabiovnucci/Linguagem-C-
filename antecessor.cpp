631e2d613018142f8531828dd59c844aa6c1e68a
fabiovnucci <fabiovnucci@gmail.com>
2022-09-10 19:57:02 -0300

    antecessor

---
+++ antecessor.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+int valor, antecessor;
+cout << "Digite um valor inteiro: ";
+cin >> valor;
+antecessor = valor -1 ;
+cout << "O antecessor do " << valor << " e: " << antecessor;
+
+
+
+}
