fe607bc189ea713633a42ad6a89075e3725861dd
fabiovnucci <fabiovnucci@gmail.com>
2022-09-10 19:52:26 -0300

    volume

---
+++ volume da caixa.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    float volume, comprimento , largura, altura ;
+    cout << "Digite o comprimento da caixa :";
+    cin >> comprimento;
+cout << "Digite o largura da :";
+    cin >> largura;
+    cout << "Digite o altura da caixa :";
+    cin >> altura;
+    volume = comprimento * largura * altura ;
+    cout << " O volume da caixa e: " << volume << " cm3 ";
+
+
+
+}
