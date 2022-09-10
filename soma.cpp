94d20a45e9ffad5864bfef60568423d69c7d8fa4
fabiovnucci <fabiovnucci@gmail.com>
2022-09-10 19:37:21 -0300

    soma

---
+++ soma.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int v1,v2, soma;
+    cout << "Digite o valor de V1: ";
+    cin >> v1;
+    cout << "Digite o valor de V2: ";
+    cin >> v2;
+    soma = v1+v2 ;
+    cout << "A soma e : " << soma ;
+}
